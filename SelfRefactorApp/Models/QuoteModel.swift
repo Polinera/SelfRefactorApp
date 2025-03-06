4dd8d089f4c98972c7f72cf55ddac8c0be37bdd7
regc <regc@demant.com>
2025-03-06 12:03:59 +0100

    wip - quotes

---
+++ SelfRefactorApp/Models/QuoteModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuoteModel.swift
+//  SelfRefactorApp
+//
+//  Created by REGC on 06/03/2025.
+//
+
+import Foundation
---
+++ SelfRefactorApp/Views/ReusableComponents/HabitStatistic.swift
@@ -0,0 +1,18 @@
+//
+//  HabitStatistic.swift
+//  SelfRefactorApp
+//
+//  Created by REGC on 06/03/2025.
+//
+
+import SwiftUI
+
+struct HabitStatistic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitStatistic()
+}
