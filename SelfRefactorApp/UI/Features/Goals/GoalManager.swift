6a11da71e594289da54746bf785c787f57a2e7e4
Renia <snrenia@gmail.com>
2025-04-14 14:42:58 +0200

    wip

--- SelfRefactorApp/UI/Features/Goals/GoalManager.swift
+++ SelfRefactorApp/UI/Features/Goals/GoalManager.swift
@@ -1,12 +1,12 @@
-class GoalManager: ObservableObject {
-    @Published var goals: [GoalModel] = [
-          GoalModel(name: "Read daily", description: "Read 20 pages of a book daily.", isCompleted: false),
-          GoalModel(name: "Run 2km", description: "Go for a 2km run every morning.", isCompleted: true)
-      ]
+//class GoalManager: ObservableObject {
+ //   @Published var goals: [GoalModel] = [
+//          GoalModel(name: "Read daily", description: "Read 20 pages of a book daily.", isCompleted: false),
+//          GoalModel(name: "Run 2km", description: "Go for a 2km run every morning.", isCompleted: true)
+//      ]
 
-    func toggleGoalCompletion(_ goal: Goal) {
-        if let index = goals.firstIndex(where: { $0.id == goal.id }) {
-            goals[index].isCompleted.toggle()
-        }
-    }
-}
+//    func toggleGoalCompletion(_ goal: Goal) {
+ //       if let index = goals.firstIndex(where: { $0.id == goal.id }) {
+ //           goals[index].isCompleted.toggle()
+ //       }
+ //   }
+//}
